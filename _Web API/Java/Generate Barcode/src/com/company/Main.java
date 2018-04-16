cc358c549be24a03f8495b41118da7ce551c100c
estar73 <estarkov@gmail.com>
2018-04-16 04:21:32 -0700

    = regular update

---
+++ _Web API/Java/Generate Barcode/src/com/company/Main.java
@@ -0,0 +1,110 @@
+//*****************************************************************************************//
+//                                                                                         //
+// Download offline evaluation version (DLL): https://bytescout.com/download/web-installer //
+//                                                                                         //
+// Signup Web API free trial: https://secure.bytescout.com/users/sign_up                   //
+//                                                                                         //
+// Copyright © 2017-2018 ByteScout Inc. All rights reserved.                               //
+// http://www.bytescout.com                                                                //
+//                                                                                         //
+//*****************************************************************************************//
+
+
+package com.company;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main
+{
+    // (!) If you are getting '(403) Forbidden' error please ensure you have set the correct API_KEY
+
+    // The authentication key (API Key).
+    // Get your own by registering at https://secure.bytescout.com/users/sign_up
+    final static String API_KEY = "***********************************";
+
+    // Result file name
+    final static Path ResultFile = Paths.get(".\\barcode.png");
+    // Barcode type. See valid barcode types in the documentation https://secure.bytescout.com/cloudapi.html#api-Default-barcodeGenerateGet
+    final static String BarcodeType = "Code128";
+    // Barcode value
+    final static String BarcodeValue = "qweasd123456";
+
+        
+    public static void main(String[] args) throws IOException
+    {
+        // Create HTTP client instance
+        OkHttpClient webClient = new OkHttpClient();
+        
+        // Prepare URL for `Barcode Generator` API call
+        String query = String.format(
+                "https://bytescout.io/v1/barcode/generate?name=%s&type=%s&value=%s",
+                ResultFile.getFileName(),
+                BarcodeType,
+                BarcodeValue);
+        
+        // Prepare request
+        Request request = new Request.Builder()
+                .url(query)
+                .addHeader("x-api-key", API_KEY) // (!) Set API Key
+                .build();
+
+        // Execute request
+        Response response = webClient.newCall(request).execute();
+
+        if (response.code() == 200)
+        {
+            // Parse JSON response
+            JsonObject json = new JsonParser().parse(response.body().string()).getAsJsonObject();
+
+            boolean error = json.get("error").getAsBoolean();
+            if (!error)
+            {
+                // Get URL of generated barcode image file
+                String resultFileUrl = json.get("url").getAsString();
+
+                // Download the image file
+                downloadFile(webClient, resultFileUrl, ResultFile);
+
+                System.out.printf("Generated barcode saved to \"%s\" file.", ResultFile.toString());
+            }
+            else
+            {
+                // Display service reported error
+                System.out.println(json.get("message").getAsString());
+            }
+        }
+        else
+        {
+            // Display request error
+            System.out.println(response.code() + " " + response.message());
+        }
+    }
+
+    public static void downloadFile(OkHttpClient webClient, String url, Path destinationFile) throws IOException
+    {
+        // Prepare request
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        // Execute request
+        Response response = webClient.newCall(request).execute();
+
+        byte[] fileBytes = response.body().bytes();
+
+        // Save downloaded bytes to file
+        OutputStream output = new FileOutputStream(destinationFile.toFile());
+        output.write(fileBytes);
+        output.flush();
+        output.close();
+
+        response.close();
+    }
+}
